69e0538a4bc99d0261ff6cd4dfa42c121801b266
nguyenlnb <nguyenlnb@ames.edu.vn>
2023-09-22 15:52:05 +0700

    add URL BE

--- src/libraries/axiosClient.js
+++ src/libraries/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: process.env.URL_BE,
   headers: { "Content-Type": "application/json" },
 });
 
